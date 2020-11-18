9ef046da98334014ca0a15fabe9c3020805819a0
Piot Maxime <piotmaxime79@hotmail.fr>
2020-11-18 17:31:10 +0100

    correction minime

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,7 +3,7 @@ let jwt = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
       let token = req.headers.authorization.split(' ')[1];
-      let decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
+      let decodedToken = jwt.verify(token, "RANDOM_TOKEN_SECRET");
       let userId = decodedToken.userId;
       if (req.body.userId && req.body.userId !== userId) {
         throw 'User ID invalide!';
